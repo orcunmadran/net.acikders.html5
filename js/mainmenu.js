fe14612734e8a8f3a54dbd7fd7f024fd52236e72
Orçun Madran <orcunmadran@gmail.com>
2022-11-19 15:32:46 +0300

    Arama formu opsiyonel hale getirildi.

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -4,3 +4,8 @@ mainMenu = [
     ['Şema', 'schema.html', '_self'],
     ['Üst Veri', 'metadata.html', '_self']
 ];
+searchForm = "";
+        //'<form class="d-flex" role="search">'+
+             //   '<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">'+
+             //   '<button class="btn btn-outline-success" type="submit">Search</button>'+
+        //'</form>';
