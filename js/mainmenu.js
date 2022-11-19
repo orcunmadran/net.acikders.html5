4d565109541127ba7048e7348ab234b69ef4381e
Orçun Madran <orcunmadran@gmail.com>
2022-11-19 15:22:45 +0300

    Ana menu oluşturuldu.

---
+++ js/mainmenu.js
@@ -0,0 +1,6 @@
+mainMenu = [
+    ['Ana Sayfa', 'index.html', '_self'],
+    ['Hakkında', 'about.html', '_self'],
+    ['Şema', 'schema.html', '_self'],
+    ['Üst Veri', 'metadata.html', '_self']
+];
